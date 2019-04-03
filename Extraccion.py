f48eef3915fa5f72bd4fb5c8daf192329e90bfa7
Graciela Rosas Sanchez <gracielarosas@grupovidanta.com>
2019-04-03 17:55:52 -0600

    Creación de bases de trabajo

--- Extraccion.py
+++ Extraccion.py
@@ -1,2 +1,31 @@
-print('Prueba')
-print('Hola')
+import pandas as pd
+import numpy as np
+
+Base_Rent = pd.read_csv('E:/Procesos Sense/Rentabilidad/RentTotalPBI_Prueba.txt')
+##print(Base_Rent.columns)
+Base_Rent = Base_Rent.drop(['Area', 'Tipo de Venta', 'Tipo_ResortFee', 'Registry',
+       'Comercializadora', 'ContractNumber',
+       'ReservationSubTypeNew', 'ReservationTypeNew', 'FirstNight',
+       'LastNight', 'Mix_Mercado', 'Mix_programa', 'Prospectable',
+       'ReservationStatus', 'Mix_Cliente', 'DateCreated', 'Nights',
+       'SiteGroup', 'RoomType', 'Wk_Mayan', 'Promotion', 'ExchangeMembership',
+       'ID', 'Id_Prom_Cam', 'Tipo_C_P', 'No Show', 'Ident_Hook',
+       'Clasificador_HK', 'Año_Mes', 'Año', 'año_Llegada', 'mes_Llegada',
+       'Monto de Venta', 'Venta', 'Reservacion',
+       'Plaza', 'Año_Ventas', 'BRM', 'CM_Noches1',
+       'Ingresos', 'Egresos', 'Benefit_SC', 'Uso_SC',
+       'Exit', 'VolumenExit', 'ComExit',
+       'AjusteSC', 'Paq_Admvo_Dolares', 
+       'Tipo de Canal', 'EstatusInnsist', 'SiteName', 'Campaign',
+       'PromotionCode', 'Actualización', 'Tipo Salto', 'Categoria', 'Unidad',
+       'Roomtype_Orig', 'Total_Saltos', 'Clasificación RF', 'Usage_Type',
+       'Clasificación_Members', 'Equity_USD', 'Intervalo', '%MemberID'], axis=1)
+print(Base_Rent.columns)
+print(np.unique(list(Base_Rent['Mix_NA_IN'])))
+
+Base_Rent_INT= Base_Rent[Base_Rent['Mix_NA_IN']=='IN']
+Base_Rent_NAT= Base_Rent[Base_Rent['Mix_NA_IN']!='IN']
+
+
+Base_Rent_INT.to_csv('Base_Trabajo_INT.csv',index=False)
+Base_Rent_NAT.to_csv('Base_Trabajo_NAT.csv',index=False)
