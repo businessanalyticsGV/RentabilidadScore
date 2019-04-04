a03ea9c832dda5d547747802c63575ff001a4e42
Alexis Saul Alvarez Soto <alexisalvarez@grupovidanta.com>
2019-04-03 18:30:48 -0600

    Inicio de scoring

--- Score.py
+++ Score.py
@@ -1,1 +1,22 @@
-import TeamAM_Score
+#### 0.- PRELIMINARIES
+
+import pandas as pd
+pd.set_option('display.max_columns',500)
+
+#### 1.- EXTRACCIÓN Y LIMPIEZA RAPIDA
+df = pd.read_csv('BaseDatos/Base_Trabajo_INT.csv')
+ls_index = ['ReservationNumber','Mix_canal']
+df_index = df[[c for c in df if c in ls_index]]
+df = df[[c for c in df if c not in ls_index]]
+df = df.drop(columns = ['Mix_NA_IN'], axis = 1)
+
+ls_egresos = ['CostoHospedaje','Comisión',
+               'Closing Cost','Promociones y Campañas','Ajuste_SC']
+
+for eg in ls_egresos:
+    df[eg] = df[eg]*-1
+
+print(df.shape)
+print(df.head())
+
+#### II.- SCORING
